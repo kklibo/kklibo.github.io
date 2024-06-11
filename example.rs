a7446c25bb63b291f52b19b61395bb6727772ef9
kklibo <kklibo@users.noreply.github.com>
2024-06-10 18:29:51 -0700

    Create example.rs

---
+++ example.rs
@@ -0,0 +1,22 @@
+pub fn merge(
+    iter: impl Iterator<Item = TokenId>,
+    merge_if: impl Fn(TokenId, TokenId) -> Option<TokenId>,
+) -> Vec<TokenId> {
+    let mut result = vec![];
+
+    let mut pairs = iter.pairs();
+    while let Some((id0, id1)) = pairs.next() {
+        if let Some(merged) = merge_if(id0, id1) {
+            result.push(merged);
+            pairs.next();
+        } else {
+            result.push(id0);
+        }
+    }
+
+    if let Some(id) = pairs.final_item() {
+        result.push(id);
+    }
+
+    result
+}
